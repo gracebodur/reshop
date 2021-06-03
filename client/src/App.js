7c76c969ad6050b82fcfd3c8aeb3557f5d4a883b
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-03 12:24:48 -0400

    Add initial Result page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Home from './pages/Home'
-import './App.css'
 import Product from './pages/Product'
+import Result from './pages/Result'
+import './App.css'
 
 const App = () => {
   return (
     <div>
       <Home />
       <Product />
+      <Result />
     </div>
   );
 }
