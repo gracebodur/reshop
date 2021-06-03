89701febfcfd3f2e9a849f618d86a8ec58be7cbd
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-03 12:17:44 -0400

    Add initial Product page using dummy image to display a product

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home'
 import './App.css'
+import Product from './pages/Product'
 
 const App = () => {
   return (
     <div>
       <Home />
+      <Product />
     </div>
   );
 }
