10efbd32d9629c6dcd5b97988ad28f49b1fba058
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-03 11:31:04 -0400

    Create -NPM scripts for running multiple commands using concurrently -adds basic configuration management

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const getRoutes = require('./routes')
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(express.json())
+
+// Serve the built version of our React app
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+app.use('api', getRoutes())
+
+app.use.apply.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
