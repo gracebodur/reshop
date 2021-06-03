7079004ede08008503f372dfff2be5957202f6f1
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-03 12:00:37 -0400

    Create and Clean up
    -remove unneeded code and files
    
    -add tailwind cdn
    
    -create initial Home page component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h1>E-Commerce App with Complete Shopping Cart</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home
+
