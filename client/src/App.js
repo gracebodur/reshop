90d10142f7811d319b2c3a862c96bc9fa8854732
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-03 13:15:27 -0400

    Set up
    -basic routing for Home, Product and Result page
    
    -configuration for auto imports of files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import Home from './pages/Home'
-import Product from './pages/Product'
-import Result from './pages/Result'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from 'pages/Home';
+import Product from 'pages/Product';
+import Result from 'pages/Result';
+
 import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <Home />
-      <Product />
-      <Result />
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/result' component={Result} />
+      <Route path='/:productId' component={Product} />
+    </Switch>
+    </Router>
   );
 }
 
