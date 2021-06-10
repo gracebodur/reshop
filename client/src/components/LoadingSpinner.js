a53ac46998a780cf8941676fa435d493eb31f4ee
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-09 23:19:34 -0400

    Change loading spinner style

--- client/src/components/LoadingSpinner.js
+++ client/src/components/LoadingSpinner.js
@@ -1,23 +1,12 @@
 import React from "react";
+import './LoadingSpinner.css'
 
 const LoadingSpinner = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <div className="lds-ring">
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
+    <div className='flex items-center justify-center h-screen'>
+      <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
-  );
+  )
 };
 
 export default LoadingSpinner;
