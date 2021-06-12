e214b5c43be401d41200068f6f14152b1e2413c9
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-11 22:11:13 -0400

    Integrate Stripe with Node and React

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,22 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import Home from "pages/Home";
 import Product from "pages/Product";
 import Result from "pages/Result";
-
+import { loadStripe } from "@stripe/stripe-js";
 import "./App.css";
+import { CartProvider } from "use-shopping-cart";
 
 const queryClient = new QueryClient();
+
+const stripePromise= loadStripe('process.env.REACT_APP_PK_TEST')
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <CartProvider
+        mode='checkout-session'
+        stripe={stripePromise}
+        currency='USD'
+      >
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
@@ -19,6 +27,7 @@ const App = () => {
           <Route path="/:productId" component={Product} />
         </Switch>
       </Router>
+      </CartProvider>
     </QueryClientProvider>
   );
 };
