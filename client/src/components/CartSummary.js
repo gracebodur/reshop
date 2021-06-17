6647c22e49dadccebddedfa009222470f99b26c4
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-17 14:22:07 -0400

    Add toggleModal function and show cart details

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,13 +1,18 @@
+// @ts-nocheck
 import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import CartModal from "./CartModal";
 import { CartIcon } from "./Icons";
 
 const CartSummary = () => {
   const { formattedTotalPrice, cartCount } = useShoppingCart();
+  const [isOpen, setOpen ] = React.useState(false)
+
+  const toggleModal = () => setOpen(!isOpen)
 
   return (
     <>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav onClick={toggleModal} className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <span className="mr-5 hover:text-white flex items-center">
           <CartIcon />
           <span className="ml-3">
@@ -15,6 +20,7 @@ const CartSummary = () => {
           </span>
         </span>
       </nav>
+      <CartModal isOpen={isOpen} toggleModal={toggleModal}/>
     </>
   );
 };
