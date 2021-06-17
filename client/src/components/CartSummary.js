981a60c6b11176b41206534d7b528b0886ca6a3e
Marie Grace Bodur <mgracebodur@gmail.com>
2021-06-17 00:35:30 -0400

    Add a cart icon component

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { CartIcon } from "./Icons";
 
 const CartSummary = () => {
   return (
     <>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <span className="mr-5 hover:text-white flex items-center">
+          <CartIcon />
           <span className="ml-3">Total Price (Cart Count)</span>
         </span>
       </nav>
     </>
   );
-}
+};
 
-export default CartSummary
+export default CartSummary;
